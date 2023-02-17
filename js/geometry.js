a084d5dd2e4b3c202c2f4293a61a03baaa3a7963
Ashik465 <helloashikul@gmail.com>
2023-02-18 02:08:33 +0600

    card-section-done

--- js/geometry.js
+++ js/geometry.js
@@ -4,4 +4,9 @@
 
 document.addEventListener("mouseover", function () {
   makeRandom("random-card-one");
+  makeRandom("random-card-two");
+  makeRandom("random-card-three");
+  makeRandom("random-card-four");
+  makeRandom("random-card-five");
+  makeRandom("random-card-six");
 });
