07f36c2375439b4ac35f2497fb727cce760d379b
Ashik465 <helloashikul@gmail.com>
2023-02-18 07:19:23 +0600

    triangle-calculation-done

--- js/geometry.js
+++ js/geometry.js
@@ -1,12 +1,43 @@
-// console.log('h')
+let serial = 0;
+// // mouse over random color
 
-// mouse over random color
+// document.addEventListener("mouseover", function () {
+//   makeRandom("random-card-one");
+//   makeRandom("random-card-two");
+//   makeRandom("random-card-three");
+//   makeRandom("random-card-four");
+//   makeRandom("random-card-five");
+//   makeRandom("random-card-six");
+// });
 
-document.addEventListener("mouseover", function () {
-  makeRandom("random-card-one");
-  makeRandom("random-card-two");
-  makeRandom("random-card-three");
-  makeRandom("random-card-four");
-  makeRandom("random-card-five");
-  makeRandom("random-card-six");
+// clicking blog button
+
+document.getElementById("blog").addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+
+// triangle card
+
+document.getElementById("triangle-btn").addEventListener("click", function () {
+  const triangleInputB = getINputValue("triangle-input-b");
+  const triangleInputH = getINputValue("triangle-input-h");
+  const Name = getInnerText("triangle-name");
+
+  const val = validation(triangleInputB,triangleInputH);
+  if(val=== false ){
+    return ;
+  }
+
+
+ serial++;
+  const triangleAreaResult = 0.5 * triangleInputB * triangleInputH;
+
+  const triangleArea = isFloat(triangleAreaResult);
+
+  //  console.log(triangleArea);
+
+
+  tableInput(serial,Name,triangleArea);
+
+  
 });
--- js/utility.js
+++ js/utility.js
@@ -10,3 +10,62 @@
 // function makeRandom(id) {
 //   document.getElementById(id).style.backgroundColor = randomColor();
 // }
+
+
+// get input value function 
+
+function getINputValue(id) {
+const inputField = document.getElementById(id).value ;
+return parseFloat(inputField);
+}
+
+
+// get inner text function 
+
+function getInnerText(id) {
+    const innerText = document.getElementById(id).innerText;
+    return innerText ;
+}
+
+// check is integer or float  function 
+
+function isFloat(area) {
+    if(Number.isInteger(area)=== false){
+    
+        return area.toFixed(2);
+        }
+        else return area ;
+}
+
+
+// validation function 
+
+function validation (firstAreaInput, secondAreaInput) {
+
+    if(firstAreaInput === "" || isNaN(firstAreaInput) || firstAreaInput < 0 || secondAreaInput === "" || isNaN(secondAreaInput) || secondAreaInput < 0) {
+        alert('please enter a valid number ') ;
+        return false;
+     }
+}
+
+
+// table input function 
+
+function tableInput(serial,Name,area){
+
+    const tableBody = document.getElementById("table-body");
+
+  const tr = document.createElement("tr");
+  
+
+  tr.innerHTML = ` <td>${serial}</td>
+   <td>${Name}</td>
+   <td>${area}cm<sup>2</sup></td>
+   <td><button 
+                  class="btn btn-primary  bg-[#1090D8] border-none"
+                >
+                  Convert to m<sup>2</sup>
+                </button></td>`;
+
+  tableBody.appendChild(tr);
+}
