21d77130666b2e0df3d19483bac55ad94f955ab5
Ashik465 <helloashikul@gmail.com>
2023-02-18 19:22:23 +0600

    all-calculation-done

--- js/geometry.js
+++ js/geometry.js
@@ -1,14 +1,14 @@
 let serial = 0;
 // // mouse over random color
 
-// document.addEventListener("mouseover", function () {
-//   makeRandom("random-card-one");
-//   makeRandom("random-card-two");
-//   makeRandom("random-card-three");
-//   makeRandom("random-card-four");
-//   makeRandom("random-card-five");
-//   makeRandom("random-card-six");
-// });
+document.addEventListener("mouseover", function () {
+  makeRandom("random-card-one");
+  makeRandom("random-card-two");
+  makeRandom("random-card-three");
+  makeRandom("random-card-four");
+  makeRandom("random-card-five");
+  makeRandom("random-card-six");
+});
 
 // clicking blog button
 
